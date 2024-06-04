72d83c3fee7f4ae66073eabaa665edafcf6660fd
Paul Vaucelle <paul.vaucelle@etu.unistra.fr>
2024-06-04 11:09:16 +0200

    Update ABCD_Run.C

--- test/ABCD_Run.C
+++ test/ABCD_Run.C
@@ -7,67 +7,210 @@
   
  if (gROOT->GetClass("ABCD")==0) return;
  
- TChain c("FlyingTop/ttree");
+ TChain c("ttree");
 
-TString Prod = "24_03_2024";
-TString Sample = "RPV_2018_smu200to500_ctau1000";
-TString Path = "/opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/"+Prod+"/"+Sample+".root";
-bool Signal = false;
-////////////////////////////////////////////////////////////////////////////////
-
-// RPV_2018_smu200to500_ctau100.root
-// RPV_2018_smu200to500_ctau300.root
-// RPV_2018_smu200to500_ctau1000.root
-// RPV_2018_smu200to500_ctau010.root
-// RPV_2018_smu200to500_ctau030.root
-// RPV_2018_smu200to500_ctau001.root
-// RPV_2018_smu200to500_ctau003.root
-
-// c.Add("../NTUPLES_FLY/CMSSW_10_6_30/MC_2017/240208_fromPaul/RPV_2017_smu200to500_ctau300.root"); // BDTtk trained on all signal samples and all SM backgrounds: /opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/2017_ProdTest/
-// c.Add("../NTUPLES_FLY/CMSSW_10_6_30/MC_2018/240209_fromPaul/RPV_2018_smu200to500_ctau300.root"); // BDTtk trained only on smu350_neu300_ctau300 and only TTTo2L2Nu: /opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/Test1SignalSample10cm/
-// c.Add("../NTUPLES_FLY/CMSSW_10_6_30/MC_2018/240212_fromPaul/RPV_2018_smu200to500_ctau300.root"); // BDTtk trained on all ctau300 samples and only TTTo2L2Nu: /opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/TestSample10cm/
-// c.Add("../NTUPLES_FLY/CMSSW_10_6_30/MC_2018/240216_fromPaul/RPV_2018_smu200to500_ctau300.root"); // BDTtk trained on all ctau300 samples and only TTTo2L2Nu: /opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/TestSample10cm/
-// c.Add("/opt/sbg/cms/ui2_data1/blochd/NTUPLES_FLY/CMSSW_10_6_30/MC_2018/240311/RPV_2018_smu250_neu200_ctau100.root"); //BDTtk trained on all ctau300 samples and only TTTo2L2Nu: /opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/TestSample10cm/
-
-// RPV_2018_smu250_neu200_ctau100
-// RPV_2018_smu300_neu180_ctau100
-// RPV_2018_smu400_neu300_ctau100
-// RPV_2018_smu500_neu350_ctau100
-
-c.Add(Path); //BDTtk trained on all ctau300 samples and only TTTo2L2Nu: /opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/TestSample10cm/
-// c.Add("/opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/BDT090_Mini/DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8.root"); //BDTtk trained on all ctau300 samples and only TTTo2L2Nu: /opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/TestSample10cm/
-// c.Add("/opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/BDT090_Mini/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8.root"); //BDTtk trained on all ctau300 samples and only TTTo2L2Nu: /opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/TestSample10cm/
-
-// DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8
-// DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8
-// TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8
-
-
-//  MiniIso
-// /opt/sbg/cms/ui2_data1/blochd/NTUPLES_FLY/CMSSW_10_6_30/MC_2018/240311
-// RPV_2018_smu200to500_ctau001.root
-// RPV_2018_smu200to500_ctau010.root
-// RPV_2018_smu200to500_ctau100.root
-// RPV_2018_smu200to500_ctau300.root
-
-//-  TkIsoTight
-// /opt/sbg/cms/ui2_data1/blochd/NTUPLES_FLY/CMSSW_10_6_30/MC_2018/240310/
-// RPV_2018_smu200to500_ctau001.root
-// RPV_2018_smu200to500_ctau010.root
-// RPV_2018_smu200to500_ctau100.root
-// RPV_2018_smu200to500_ctau300.root
-
-// c.Add("../NTUPLES_FLY/CMSSW_10_6_30/MC_2018/240309/RPV_2018_smu300_neu200_ctau300.root");
+TString Prod = "DATA_2018_29_05_2024/095";
+// TString Path = "/opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/"+Prod+"/"+Sample+".root";
+// bool Signal = true;
 
 ////////////////////////////////////////////////////////////////////////////////
+//----DATA----//
+TString DataSet[4]={ "2018A","2018B","2018C","2018D"};
 
- ABCD* t = new ABCD(&c);
+ for (int i = 0 ; i< 4 ; i++) 
+    {
+      TString Path = "/opt/sbg/cms/ui2_data1/blochd/NTUPLES_FLY/Data_2018_emu/240529_BDTcut95/Mini"+DataSet[i]+".root";
+      c.Reset();
+      c.Add(Path);
+      ABCD* t = new ABCD(&c);
+      t->Loop(DataSet[i],Prod,false);
+    }
 
-// t->Loop(0,180.,20.,2999.,0.0,10.,0.,1,"output/h_RPV_2018_smu200to500_ctau300_240201_2vtxloose.root","");
-// t->Loop(0,180.,20.,2999.,0.0,10.,0.,1,"output/h_RPV_2018_smu200to500_ctau300_240211_swapNegIpTk_2vtx.root","");
 
-t->Loop(0,180.,20.,2999.,0.0,10.,0.,1,"./"+Prod+"/h_"+Sample+"_REF.root","",Sample,Prod,Signal);
-// t->Loop(0,180.,20.,2999.,0.0,10.,0.,1,"output/h_RPV_2018_smu200to500_ctau300_240309_noCloseVtx.root","");
+// //--------------------Background MiniNtuples ------------//
+//   TString BKGSet[13]={
+//                       "TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8","TTToHadronic_TuneCP5CR1_13TeV-powheg-pythia8","TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8"
+//                       ,"ST_tW_top_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8","ST_tW_antitop_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8",
+//                       "DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8","DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8",
+//                       "WWTo2L2Nu_TuneCP5_13TeV-powheg-pythia8","WZTo2Q2L_mllmin4p0_TuneCP5_13TeV-amcatnloFXFX-pythia8","ZZTo2Q2L_mllmin4p0_TuneCP5_13TeV-amcatnloFXFX-pythia8",
+//                       "ttWJetsToLNu_5f_EWK_TuneCP5_13TeV_amcatnlo-pythia8","TTZToLL_5f_TuneCP5_13TeV-madgraphMLM-pythia8","TTWW_TuneCP5_13TeV-madgraph-pythia8"
+//   };
+
+     
+//  for (int i = 0 ; i< 13 ; i++) 
+//     {
+//       TString Path = "/opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/"+Prod+"/Mini"+BKGSet[i]+".root";
+//       c.Reset();
+//       c.Add(Path);
+//       ABCD* t = new ABCD(&c);
+//       t->Loop(BKGSet[i],Prod,false);
+//     }
+
+// ////////////////////////////////////////////////////////////////////////////////
+
+
+// // ////////////////////////////////////////////////////////////////////////////////
+// // //--------------------Test Signal MiniNtuples ------------//
+// TString SignalSet[4]={"RPV_2018_smu300_neu250_ctau100","RPV_2018_smu400_neu300_ctau100","RPV_2018_smu250_neu200_ctau100","RPV_2018_smu500_neu350_ctau100"};
+
+
+//  for (int i = 0 ; i< 4 ; i++) 
+//     {
+//       TString Path = "/opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/"+Prod+"/Mini"+SignalSet[i]+".root";
+//       c.Reset();
+//       c.Add(Path);
+//        ABCD* t = new ABCD(&c);
+//        t->Loop(SignalSet[i],Prod,true);
+//     }
+
+// //--------------------------------------------------//
+
+
+// // ////////////////////////////////////////////////////////////////////////////////
+// // //--------------------Test DATA-----------//
+// TString SignalSet[4]={"RPV_2018_smu300_neu250_ctau100","RPV_2018_smu400_neu300_ctau100","RPV_2018_smu250_neu200_ctau100","RPV_2018_smu500_neu350_ctau100"};
+
+
+//  for (int i = 0 ; i< 4 ; i++) 
+//     {
+//       TString Path = "/opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/"+Prod+"/Mini"+SignalSet[i]+".root";
+//       c.Reset();
+//       c.Add(Path);
+//        ABCD* t = new ABCD(&c);
+//        t->Loop(SignalSet[i],Prod,true);
+//     }
+
+//--------------------------------------------------//
+
+
+//       TString SignalSet001[32]={"RPV_2018_smu200_neu180_ctau001","RPV_2018_smu250_neu180_ctau001","RPV_2018_smu250_neu200_ctau001",
+// "RPV_2018_smu250_neu230_ctau001","RPV_2018_smu300_neu180_ctau001","RPV_2018_smu300_neu200_ctau001","RPV_2018_smu300_neu280_ctau001",
+// "RPV_2018_smu350_neu180_ctau001","RPV_2018_smu350_neu200_ctau001","RPV_2018_smu350_neu250_ctau001","RPV_2018_smu350_neu300_ctau001","RPV_2018_smu350_neu330_ctau001",
+// "RPV_2018_smu400_neu180_ctau001","RPV_2018_smu400_neu200_ctau001","RPV_2018_smu400_neu250_ctau001","RPV_2018_smu400_neu300_ctau001","RPV_2018_smu400_neu350_ctau001",
+// "RPV_2018_smu400_neu380_ctau001","RPV_2018_smu450_neu180_ctau001","RPV_2018_smu450_neu200_ctau001","RPV_2018_smu450_neu250_ctau001","RPV_2018_smu450_neu300_ctau001",
+// "RPV_2018_smu450_neu350_ctau001","RPV_2018_smu450_neu400_ctau001","RPV_2018_smu450_neu430_ctau001","RPV_2018_smu500_neu180_ctau001","RPV_2018_smu500_neu200_ctau001",
+// "RPV_2018_smu500_neu250_ctau001","RPV_2018_smu500_neu300_ctau001","RPV_2018_smu500_neu350_ctau001",/*"RPV_2018_smu500_neu400_ctau001","RPV_2018_smu500_neu450_ctau001",*/
+// "RPV_2018_smu500_neu480_ctau001"};     
+ 
+//  for (int i = 0 ; i< 31 ; i++) 
+//     {
+//       TString Path = "/opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/"+Prod+"/"+SignalSet[i]+".root";
+//       c.Reset();
+//       c.Add(Path);
+//        ABCD* t = new ABCD(&c);
+//        t->Loop(SignalSet[i],Prod,Signal);
+//     }
+
+// ////////////////////////////////////////////////////////////////////////////////
+
+//       TString SignalSet003[33]={"RPV_2018_smu200_neu180_ctau003","RPV_2018_smu250_neu180_ctau003","RPV_2018_smu250_neu200_ctau003",
+// "RPV_2018_smu250_neu230_ctau003","RPV_2018_smu300_neu180_ctau003","RPV_2018_smu300_neu200_ctau003","RPV_2018_smu300_neu280_ctau003",
+// "RPV_2018_smu350_neu180_ctau003","RPV_2018_smu350_neu200_ctau003","RPV_2018_smu350_neu250_ctau003","RPV_2018_smu350_neu300_ctau003","RPV_2018_smu350_neu330_ctau003",
+// "RPV_2018_smu400_neu180_ctau003","RPV_2018_smu400_neu200_ctau003","RPV_2018_smu400_neu250_ctau003","RPV_2018_smu400_neu300_ctau003","RPV_2018_smu400_neu350_ctau003",
+// "RPV_2018_smu400_neu380_ctau003","RPV_2018_smu450_neu180_ctau003","RPV_2018_smu450_neu200_ctau003","RPV_2018_smu450_neu250_ctau003","RPV_2018_smu450_neu300_ctau003",
+// "RPV_2018_smu450_neu350_ctau003","RPV_2018_smu450_neu400_ctau003","RPV_2018_smu450_neu430_ctau003","RPV_2018_smu500_neu180_ctau003","RPV_2018_smu500_neu200_ctau003",
+// "RPV_2018_smu500_neu250_ctau003","RPV_2018_smu500_neu300_ctau003","RPV_2018_smu500_neu350_ctau003",/*"RPV_2018_smu500_neu400_ctau003",*/"RPV_2018_smu500_neu450_ctau003",
+// "RPV_2018_smu500_neu480_ctau003"};
+
+//  for (int i = 0 ; i< 32 ; i++) 
+//     {
+//       TString Path = "/opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/"+Prod+"/"+SignalSet[i]+".root";
+//       c.Reset();
+//       c.Add(Path);
+//        ABCD* t = new ABCD(&c);
+//        t->Loop(SignalSet[i],Prod,Signal);
+//     }
+
+//       TString SignalSet010[32]={"RPV_2018_smu200_neu180_ctau010","RPV_2018_smu250_neu180_ctau010","RPV_2018_smu250_neu200_ctau010",
+// "RPV_2018_smu250_neu230_ctau010","RPV_2018_smu300_neu180_ctau010","RPV_2018_smu300_neu200_ctau010","RPV_2018_smu300_neu280_ctau010",
+// "RPV_2018_smu350_neu180_ctau010","RPV_2018_smu350_neu200_ctau010","RPV_2018_smu350_neu250_ctau010","RPV_2018_smu350_neu300_ctau010","RPV_2018_smu350_neu330_ctau010",
+// "RPV_2018_smu400_neu180_ctau010","RPV_2018_smu400_neu200_ctau010","RPV_2018_smu400_neu250_ctau010","RPV_2018_smu400_neu300_ctau010","RPV_2018_smu400_neu350_ctau010",
+// "RPV_2018_smu400_neu380_ctau010","RPV_2018_smu450_neu180_ctau010","RPV_2018_smu450_neu200_ctau010","RPV_2018_smu450_neu250_ctau010","RPV_2018_smu450_neu300_ctau010",
+// "RPV_2018_smu450_neu350_ctau010","RPV_2018_smu450_neu400_ctau010","RPV_2018_smu450_neu430_ctau010","RPV_2018_smu500_neu180_ctau010","RPV_2018_smu500_neu200_ctau010",
+// "RPV_2018_smu500_neu250_ctau010","RPV_2018_smu500_neu300_ctau010",/*"RPV_2018_smu500_neu350_ctau010","RPV_2018_smu500_neu400_ctau010",*/"RPV_2018_smu500_neu450_ctau010",
+// "RPV_2018_smu500_neu480_ctau010"};
+
+//  for (int i = 0 ; i< 31 ; i++) 
+//     {
+//       TString Path = "/opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/"+Prod+"/"+SignalSet[i]+".root";
+//       c.Reset();
+//       c.Add(Path);
+//        ABCD* t = new ABCD(&c);
+//        t->Loop(SignalSet[i],Prod,Signal);
+//     }
+
+//       TString SignalSet030[34]={"RPV_2018_smu200_neu180_ctau030","RPV_2018_smu250_neu180_ctau030","RPV_2018_smu250_neu200_ctau030",
+// "RPV_2018_smu250_neu230_ctau030","RPV_2018_smu300_neu180_ctau030","RPV_2018_smu300_neu200_ctau030","RPV_2018_smu300_neu280_ctau030",
+// "RPV_2018_smu350_neu180_ctau030","RPV_2018_smu350_neu200_ctau030","RPV_2018_smu350_neu250_ctau030","RPV_2018_smu350_neu300_ctau030","RPV_2018_smu350_neu330_ctau030",
+// "RPV_2018_smu400_neu180_ctau030","RPV_2018_smu400_neu200_ctau030","RPV_2018_smu400_neu250_ctau030","RPV_2018_smu400_neu300_ctau030","RPV_2018_smu400_neu350_ctau030",
+// "RPV_2018_smu400_neu380_ctau030","RPV_2018_smu450_neu180_ctau030","RPV_2018_smu450_neu200_ctau030","RPV_2018_smu450_neu250_ctau030","RPV_2018_smu450_neu300_ctau030",
+// "RPV_2018_smu450_neu350_ctau030","RPV_2018_smu450_neu400_ctau030","RPV_2018_smu450_neu430_ctau030","RPV_2018_smu500_neu180_ctau030","RPV_2018_smu500_neu200_ctau030",
+// "RPV_2018_smu500_neu250_ctau030","RPV_2018_smu500_neu300_ctau030","RPV_2018_smu500_neu350_ctau030","RPV_2018_smu500_neu400_ctau030","RPV_2018_smu500_neu450_ctau030",
+// "RPV_2018_smu500_neu480_ctau030"};
+
+//  for (int i = 0 ; i< 33 ; i++) 
+//     {
+//       TString Path = "/opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/"+Prod+"/"+SignalSet[i]+".root";
+//       c.Reset();
+//       c.Add(Path);
+//        ABCD* t = new ABCD(&c);
+//        t->Loop(SignalSet[i],Prod,Signal);
+//     }
+
+//     TString SignalSet100[32]={"RPV_2018_smu200_neu180_ctau100","RPV_2018_smu250_neu180_ctau100","RPV_2018_smu250_neu200_ctau100",
+// "RPV_2018_smu250_neu230_ctau100","RPV_2018_smu300_neu180_ctau100",/*"RPV_2018_smu300_neu200_ctau100","RPV_2018_smu300_neu250_ctau100_v3",*/"RPV_2018_smu300_neu280_ctau100",
+// "RPV_2018_smu350_neu180_ctau100","RPV_2018_smu350_neu200_ctau100","RPV_2018_smu350_neu250_ctau100","RPV_2018_smu350_neu300_ctau100","RPV_2018_smu350_neu330_ctau100",
+// "RPV_2018_smu400_neu180_ctau100","RPV_2018_smu400_neu200_ctau100","RPV_2018_smu400_neu250_ctau100","RPV_2018_smu400_neu300_ctau100","RPV_2018_smu400_neu350_ctau100",
+// "RPV_2018_smu400_neu380_ctau100","RPV_2018_smu450_neu180_ctau100","RPV_2018_smu450_neu200_ctau100","RPV_2018_smu450_neu250_ctau100","RPV_2018_smu450_neu300_ctau100",
+// "RPV_2018_smu450_neu350_ctau100","RPV_2018_smu450_neu400_ctau100","RPV_2018_smu450_neu430_ctau100","RPV_2018_smu500_neu180_ctau100","RPV_2018_smu500_neu200_ctau100",
+// "RPV_2018_smu500_neu250_ctau100","RPV_2018_smu500_neu300_ctau100","RPV_2018_smu500_neu350_ctau100","RPV_2018_smu500_neu400_ctau100","RPV_2018_smu500_neu450_ctau100",
+// "RPV_2018_smu500_neu480_ctau100"};
+
+//  for (int i = 0 ; i< 31 ; i++) 
+//     {
+//       TString Path = "/opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/"+Prod+"/"+SignalSet[i]+".root";
+//       c.Reset();
+//       c.Add(Path);
+//        ABCD* t = new ABCD(&c);
+//        t->Loop(SignalSet[i],Prod,Signal);
+//     }
+
+//       TString SignalSet300[34]={"RPV_2018_smu200_neu180_ctau300","RPV_2018_smu250_neu180_ctau300","RPV_2018_smu250_neu200_ctau300",
+// "RPV_2018_smu250_neu230_ctau300","RPV_2018_smu300_neu180_ctau300","RPV_2018_smu300_neu200_ctau300","RPV_2018_smu300_neu280_ctau300",
+// "RPV_2018_smu350_neu180_ctau300","RPV_2018_smu350_neu200_ctau300","RPV_2018_smu350_neu250_ctau300","RPV_2018_smu350_neu300_ctau300","RPV_2018_smu350_neu330_ctau300",
+// "RPV_2018_smu400_neu180_ctau300","RPV_2018_smu400_neu200_ctau300","RPV_2018_smu400_neu250_ctau300","RPV_2018_smu400_neu300_ctau300","RPV_2018_smu400_neu350_ctau300",
+// "RPV_2018_smu400_neu380_ctau300","RPV_2018_smu450_neu180_ctau300","RPV_2018_smu450_neu200_ctau300","RPV_2018_smu450_neu250_ctau300","RPV_2018_smu450_neu300_ctau300",
+// "RPV_2018_smu450_neu350_ctau300","RPV_2018_smu450_neu400_ctau300","RPV_2018_smu450_neu430_ctau300","RPV_2018_smu500_neu180_ctau300","RPV_2018_smu500_neu200_ctau300",
+// "RPV_2018_smu500_neu250_ctau300","RPV_2018_smu500_neu300_ctau300","RPV_2018_smu500_neu350_ctau300","RPV_2018_smu500_neu400_ctau300","RPV_2018_smu500_neu450_ctau300",
+// "RPV_2018_smu500_neu480_ctau300"};
+
+//  for (int i = 0 ; i< 33 ; i++) 
+//     {
+//       TString Path = "/opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/"+Prod+"/"+SignalSet[i]+".root";
+//       c.Reset();
+//       c.Add(Path);
+//        ABCD* t = new ABCD(&c);
+//        t->Loop(SignalSet[i],Prod,Signal);
+//     }
+
+//           TString SignalSet1000[33]={"RPV_2018_smu200_neu180_ctau1000","RPV_2018_smu250_neu180_ctau1000","RPV_2018_smu250_neu200_ctau1000",
+// /*"RPV_2018_smu250_neu230_ctau1000",*/"RPV_2018_smu300_neu180_ctau1000","RPV_2018_smu300_neu200_ctau1000","RPV_2018_smu300_neu280_ctau1000",
+// "RPV_2018_smu350_neu180_ctau1000","RPV_2018_smu350_neu200_ctau1000","RPV_2018_smu350_neu250_ctau1000","RPV_2018_smu350_neu300_ctau1000","RPV_2018_smu350_neu330_ctau1000",
+// "RPV_2018_smu400_neu180_ctau1000","RPV_2018_smu400_neu200_ctau1000","RPV_2018_smu400_neu250_ctau1000","RPV_2018_smu400_neu300_ctau1000","RPV_2018_smu400_neu350_ctau1000",
+// "RPV_2018_smu400_neu380_ctau1000","RPV_2018_smu450_neu180_ctau1000","RPV_2018_smu450_neu200_ctau1000","RPV_2018_smu450_neu250_ctau1000","RPV_2018_smu450_neu300_ctau1000",
+// "RPV_2018_smu450_neu350_ctau1000","RPV_2018_smu450_neu400_ctau1000","RPV_2018_smu450_neu430_ctau1000","RPV_2018_smu500_neu180_ctau1000","RPV_2018_smu500_neu200_ctau1000",
+// "RPV_2018_smu500_neu250_ctau1000","RPV_2018_smu500_neu300_ctau1000","RPV_2018_smu500_neu350_ctau1000","RPV_2018_smu500_neu400_ctau1000","RPV_2018_smu500_neu450_ctau1000",
+// "RPV_2018_smu500_neu480_ctau1000"};
+
+//  for (int i = 0 ; i< 32 ; i++) 
+//     {
+//       TString Path = "/opt/sbg/cms/ui2_data1/pvaucell/CMSSW_10_6_30_FLY/src/FlyingTop/FlyingTop/test/"+Prod+"/"+SignalSet[i]+".root";
+//       c.Reset();
+//       c.Add(Path);
+//        ABCD* t = new ABCD(&c);
+//        t->Loop(SignalSet[i],Prod,Signal);
+//     }
+
 
 ////////////////////////////////////////////////////////////////////////////////
 
